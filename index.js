ac2cfe75fb09496908f435379481c1d29193cfe3
indianstar <bhagyshri541@gmail.com>
2022-11-19 19:54:56 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,22 +5,3 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-function multiplication()
-{
-var b1= parseInt(document.getElementById("box1").value);
-var b2= parseInt(document.getElementById("box2").value);
-  var table = document.getElementById ('multable');
-  var output= "";
-
-  for( var i=1;i<=b1;i++)
-  {
-    output+= "<tr>";
-      for(var j=1;j<= b2 ;j++)
-      {
-        output += "<td>" + i*j+"</td>";
-      }
-      output +="</tr>"
-    }
-    table.innerHTML = output;
-
-}
